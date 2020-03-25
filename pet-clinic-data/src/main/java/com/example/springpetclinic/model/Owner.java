ba78b6c8caba12336acfb4e839979e0406cd99ec
CharanNK <charkeet@gmail.com>
2020-03-25 23:24:18 +0530

    adding contact info properties to owners. closes #27

--- pet-clinic-data/src/main/java/com/example/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/model/Owner.java
@@ -11,9 +11,47 @@ public class Owner extends Person {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    private String address;
+    private String city;
+    private String telephone;
+
     protected String lastName;
 
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     public Set<Pet> getPets() {
         return pets;
